b120614a33646f79e51cd7b34a8646b6ea8955c9
jekaterinak <jekaterina.kuzmina@gmail.com>
2018-04-11 23:24:19 +0300

    added test case for new contact creation

---
+++ contact.py
@@ -0,0 +1,15 @@
+class Contact:
+    def __init__(self, firstname, lastname, address, homephone, mobilephone, workphone, fax, email, email2, email3):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+
+
+
