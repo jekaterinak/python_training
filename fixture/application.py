ab5650f864a1265eaaa8fb81896e506979bf40ba
jekaterinak <jekaterina.kuzmina@gmail.com>
2018-05-11 17:30:56 +0300

    adding option from command-line

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,11 +7,19 @@ import re
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,12 +31,12 @@ class Application:
     def open_home_page(self):
         if not (self.wd.current_url.endswith("/addressbook/") and len(
                 self.wd.find_elements_by_id("search-az")) > 0):
-            self.wd.get("http://localhost/addressbook/")
+            self.wd.get(self.base_url)
 
-    def clear_phones(self,s):
+    def clear_phones(self, s):
         return re.sub("[() -]", "", s)
 
-    def remove_spaces(self,t):
+    def remove_spaces(self, t):
         trimmed = t.strip()
         return re.sub("[ ]+", " ", trimmed)
 
