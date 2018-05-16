ee6c77c7e3092c0c1abecbab83a847802c1eafbb
jekaterinak <jekaterina.kuzmina@gmail.com>
2018-05-16 21:24:11 +0300

    test data loading from json file

---
+++ data/contacts.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="firstname1", lastname="lastname1", address="address1", homephone="1",
+            mobilephone="2", workphone="3", email="email1", email2="email1", email3="email1"),
+    Contact(firstname="firstname2", lastname="lastname2", address="address2", homephone="1",
+            mobilephone="2", workphone="3", email="email2", email2="email2", email3="email2")
+
+]
--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import string
 import random
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -39,4 +39,5 @@ testdata = [Contact(firstname="", lastname="", address="", homephone="",
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json",indent=2)
+    out.write(jsonpickle.encode(testdata))
