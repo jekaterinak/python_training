529994fe460be56d5a194d9859af0794b9222bf1
jekaterinak <jekaterina.kuzmina@gmail.com>
2018-06-10 17:07:32 +0300

    modified add group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,7 +8,6 @@ def test_add_group(app, db, json_groups, check_ui):
     old_groups = db.get_group_list()
     app.group.create(group)
     new_groups = db.get_group_list()
-    group.name = re.sub("[ ]+", " ", group.name)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
